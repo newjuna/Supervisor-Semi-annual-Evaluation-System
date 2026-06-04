3180d8d71371ad5097fb5b3a4e14efe528efb952
newjuna <zxcv6309@gmail.com>
2026-06-04 11:20:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
  *
  * 사용 전 반드시 아래 APPS_SCRIPT_URL을 본인의 Apps Script 웹앱 URL로 변경하세요.
  */
-const APPS_SCRIPT_URL = '여기에_Apps_Script_웹앱_URL을_붙여넣으세요';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbwHBtx6sIBF9FggCXqlzBwFwXm9O7qSvNkusxKmv_9gAjvoHmpfO23ITe11CPtIsuOW/exec';
 
 const EVALUATION_ITEMS = [
   {
