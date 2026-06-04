119f6b5acfeef5d9cb5e038f291d527039ab7a45
newjuna <zxcv6309@gmail.com>
2026-06-04 17:04:31 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
  *
  * 사용 전 반드시 아래 APPS_SCRIPT_URL을 본인의 Apps Script 웹앱 URL로 변경하세요.
  */
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzViJBIx661VWBak3cvQqCpqTpKWwJ9ekeF8SUrMRgjpjP2sDzrVqpwO96uijbINVBc/exec;
+const APPS_SCRIPT_URL = '여기에_Apps_Script_웹앱_URL을_붙여넣으세요';
 
 const EVALUATION_ITEMS = [
   {
