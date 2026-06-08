c9d7a1cbbdf0fabaaca988f272d746b2c7c9ae0d
newjuna <zxcv6309@gmail.com>
2026-06-08 15:41:57 +0900

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
  *
  * 사용 전 반드시 아래 APPS_SCRIPT_URL을 본인의 Apps Script 웹앱 URL로 변경하세요.
  */
-const APPS_SCRIPT_URL = '여기에_Apps_Script_웹앱_URL을_붙여넣으세요';
+const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbxdfvjQFmCiKkkV3QuQT2D6FTpKM4Yt-7catMU7vWxfcyOtt6SceAba0hsrnUxNTzJR/exec';
 const IMAGE_COMPRESSION_CONFIG = {
   targetDataUrlLength: 260000,
   maxDataUrlLength: 360000,
