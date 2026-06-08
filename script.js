36e65af4a6adb0f111c8f529af2b9d8ef7a482d8
newjuna <zxcv6309@gmail.com>
2026-06-08 11:38:46 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -10,7 +10,7 @@
  *
  * 사용 전 반드시 아래 APPS_SCRIPT_URL을 본인의 Apps Script 웹앱 URL로 변경하세요.
  */
-const APPS_SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzm417eD9MAmzLf3ZQ61VAH1GdZLLvXnBbe6sqdk44--jF7m_aSCFRPDj7XF2vf3bLY/exec';
+const APPS_SCRIPT_URL = '여기에_Apps_Script_웹앱_URL을_붙여넣으세요';
 const IMAGE_COMPRESSION_CONFIG = {
   targetDataUrlLength: 260000,
   maxDataUrlLength: 360000,
